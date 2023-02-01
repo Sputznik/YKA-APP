ef92c9987f015f0ffe8a7e1bfe7483124325c50e
psychome004 <anil.stephen66@gmail.com>
2023-02-01 16:09:46 +0530

    Add vue-query plugin options

--- src/main.js
+++ src/main.js
@@ -5,7 +5,21 @@ import store from "./store";
 import "./registerServiceWorker";
 import "./assets/tailwind.css";
 import "./assets/global.css";
+import { VueQueryPlugin } from "vue-query";
+import axios from "axios";
+import VueAxios from "vue-axios";
+
 import OrbitPosts from "@/lib/OrbitPosts.vue";
+
+const VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  },
+};
 
 const app = createApp(App);
 
@@ -13,4 +27,9 @@ app.component("OrbitPosts", OrbitPosts);
 
 // createApp(App).use(store).use(router).mount("#app");
 
-app.use(store).use(router).mount("#app");
+app
+  .use(VueQueryPlugin, VueQueryPluginOptions)
+  .use(VueAxios, axios)
+  .use(store)
+  .use(router)
+  .mount("#app");
