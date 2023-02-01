f5efd121dbad4ff1414d54691c930c9d37a7902e
psychome004 <anil.stephen66@gmail.com>
2023-02-01 17:16:00 +0530

    Update homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,21 +6,38 @@
       </router-link>
     </template>
     <template v-slot:appbody>
-      <div class="home">HOMEPAGE</div>
+      <div class="home">
+        <OrbitPosts :params="{ style: 'PostList', pagination: 1 }">
+          <template v-slot:loadingAnimation>
+            <PostListAnimation />
+          </template>
+          <template v-slot:nextPageAnimation>
+            <LoadMore />
+          </template>
+          <template v-slot:whenempty>
+            <div class="text-xs text-red border border-red p-2 mt-4">
+              Sorry, we could not find any articles
+            </div>
+          </template>
+        </OrbitPosts>
+      </div>
     </template>
   </AppUI>
 </template>
 
 <script>
-// @ is an alias to /src
 import Icon from "@/components/Icon.vue";
+import LoadMore from "@/components/LoadMore.vue";
 import AppUI from "@/components/app-ui/AppUI.vue";
+import PostListAnimation from "@/templates/post-list/PostListAnimation.vue";
 
 export default {
   name: "Home",
   components: {
     AppUI,
     Icon,
+    LoadMore,
+    PostListAnimation,
   },
 };
 </script>
