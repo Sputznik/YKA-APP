6a2b5a0296d3af3c220d218c80b3f06fcfde7e63
psychome004 <anil.stephen66@gmail.com>
2023-07-17 13:44:09 +0530

    Add single-base component

---
+++ src/lib/SingleBase.vue
@@ -0,0 +1,32 @@
+<template>
+  <component
+    :is="component"
+    :post="post"
+    :key="post.slug"
+    status="success"
+  ></component>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+export default {
+  name: "SingleBase",
+  props: {
+    type: String,
+    template: String,
+  },
+  setup(props) {
+    var component = ref(props.template);
+    const route = useRoute();
+
+    const post = JSON.parse(history.state.post);
+
+    return {
+      post,
+      component,
+    };
+  },
+};
+</script>
